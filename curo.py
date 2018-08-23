1a467df5929450f600b208cef5351949c9712df6
was421 <was421.1@gmail.com>
2018-08-23 10:41:37 -0500

    INIT

---
+++ curo.py
@@ -0,0 +1,23 @@
+import discord
+
+TOKEN = ""
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.metion}'.format(message)
+        await client.send_message(message.channe, msg)
+        
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
